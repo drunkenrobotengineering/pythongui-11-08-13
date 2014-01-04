3bcd841d4e37b144b9977658ba737cf0c3e12b64
Steve Norum <stevenorum@gmail.com>
2014-01-04 15:26:50 -0500

    Wrote python code to interact with DynamoDB.
    Added keg_interface.py, which listens to the Arduino over serial.  When drink information is sent, it gets parsed and stored in DynamoDB.  The individual drinks are recorded, and the user's lifetime drink totals get updated to ease in the website's job of displaying the data.
    Still need to write the parser that reads from DynamoDB and generates the static web page every so often.  That seems easier than having it be dynamic, so I'll do the static version first.

---
+++ keg_interface.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+from datetime import datetime
+import json
+import os
+import sys
+import time
+
+from boto.dynamodb2.items import Item
+from boto.dynamodb2.table import Table
+from lib.arduino_io import arduino, fake_arduino
+
+drinkers = Table('drinkers')
+drinks = Table('drinks')
+
+def get_drinker(code):
+    drinker = drinkers.get_item(code=code)
+    if (drinker == None):
+        drinker = Item(drinkers, data={
+                'code': code,
+                'volume_consumed': 0,
+                'number_of_drinks' : 0
+                })
+    if (drinker['code'] == None):
+        drinker['code'] = code
+    if (drinker['volume_consumed'] == None):
+        drinker['volume_consumed'] = 0
+    if (drinker['number_of_drinks'] == None):
+        drinker['number_of_drinks'] = 0
+
+    return drinker
+                    
+def update_dynamo(values):
+    # Tables:
+    # DRINK_RECORD
+    # -code : hash key
+    # -timestamp : range key
+    # -amount
+    # -tap
+    # USER_TOTALS
+    # -code : hash key
+    # -number_of_drinks
+    # -volume_consumed
+    code = values["CODE"]
+    tap_one = values["TAP_ONE"]
+    tap_two = values["TAP_TWO"]
+    total_amount = tap_one + tap_two
+    timestamp = long(time.time())
+    if (total_amount > 0):
+        drinker = get_drinker(code)
+        if (tap_one > 0):
+            drinker['volume_consumed'] = drinker['volume_consumed'] + int(tap_one)
+            drinker['number_of_drinks'] = drinker['number_of_drinks'] + 1
+            drink_1 = Item(drinks, data={
+                    'code': code,
+                    'timestamp': timestamp,
+                    'amount': int(tap_one),
+                    'tap' : 1
+                    })
+            drink_1.save()
+            timestamp = timestamp + 1 # Not ideal, but this lets us store both drinks separately, as the code/timestamp combination must be unique
+        if (tap_two > 0):
+            drinker['volume_consumed'] = drinker['volume_consumed'] + int(tap_two)
+            drinker['number_of_drinks'] = drinker['number_of_drinks'] + 1
+            drink_2 = Item(drinks, data={
+                    'code': code,
+                    'timestamp': timestamp,
+                    'amount': int(tap_two),
+                    'tap' : 2
+                    })
+            drink_2.save()
+        drinker.save()
+        print("User " + code + " drank " + str(tap_one) + " mL from tap one and " + str(tap_two) + " mL from tap two at " + datetime.fromtimestamp(timestamp).strftime("%I:%M%p on %A, %d %B %Y"))
+    else:
+        print("User " + code + " tapped their badge without drinking at " + datetime.fromtimestamp(timestamp).strftime("%I:%M%p on %A, %d %B %Y"))
+
+if __name__ == "__main__":
+    arduino = arduino()
+    #arduino = fake_arduino()
+    while ( True ) :
+        input = arduino.await_input()
+        values = json.loads(input)
+        update_dynamo(values)
--- lib/arduino_io.py
+++ lib/arduino_io.py
@@ -4,23 +4,19 @@ import json
 import serial
 from serial import *
 import time
+import random
 
 class arduino():
-    serial_port = "/dev/tty.arduino"
-#    serial_port = "/dev/tty.usbmodem1411"
-    def __init__(self, timeout=5, writeTimeout=5):
+#    serial_port = "/dev/tty.arduino"
+    serial_port = "/dev/tty.usbmodem1411"
+    def __init__(self, timeout=None, writeTimeout=5):
         self.serial = serial.Serial(port=self.serial_port, baudrate=19200)
         time.sleep(3)
 
-    def read_drink_info_from_serial(self):
-        try:
-            self.serial.write("1")
-            input = self.serial.readline()
-            print("Received serial input: " + input)
-            drink_info = json.loads(input)
-            return drink_info
-        except SerialTimeoutException as e:
-            return None
+    def await_input(self):
+        input = self.serial.readline()
+#            print("Received serial input: " + input)
+        return input
 
 class fake_arduino():
     # For testing when I don't have an Arduino handy
@@ -29,9 +25,12 @@ class fake_arduino():
     def __init__(self):
         pass
 
-    def read_drink_info_from_serial(self):
+    def await_input(self):
+        delay = random.randint(10,30)
+        time.sleep(delay)
         self.one = self.one + 20
         self.two = self.two * 1.02
+        return '{"CODE":"12345abcde", "TAP_ONE": 123, "TAP_TWO":234}'
         drink_info={"1":{},"2":{}}
         drink_info["1"]["c"] = self.one
         drink_info["2"]["c"] = self.two
